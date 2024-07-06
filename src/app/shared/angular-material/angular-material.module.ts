f0c71fc563b7d959a63592501bbca777a2c58f95
henriqtorresl <henriquetlandin@gmail.com>
2024-07-06 16:20:15 -0300

    adicionando modulos para o side nav

--- src/app/shared/angular-material/angular-material.module.ts
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatSelectModule
   ]
 })
 export class AngularMaterialModule { }
