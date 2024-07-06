51e30581fe52d6e3b1a840fc70f4e62f7f75ffc4
henriqtorresl <henriquetlandin@gmail.com>
2024-07-06 17:37:08 -0300

    funcionalidade do side bar finalizada

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+
+  isSidebarOpen = false;
+
+  constructor(
+    private elementRef: ElementRef
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+  onMenuButtonClick(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isSidebarOpen = false; // Fecha a sidebar se o clique for fora dela
+    }
+  }
 
 }
