c80f05f00d453df3ed45380b2896f5551b7a8153
henriqtorresl <henriquetlandin@gmail.com>
2024-07-08 16:14:06 -0300

    versão mobile de contato finalizada

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DarkThemeService } from 'src/app/services/dark-theme.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+
+  email: string = 'henriquetlandin@gmail.com';
+  number: string = '(61) 99886-0013';
+
+
+  constructor(
+    public darkThemeService: DarkThemeService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  copyText(): void {
+    this.snackBar.open('Texto copiado!', 'OK', {
+      duration: 1000
+    });
+  }
 
 }
