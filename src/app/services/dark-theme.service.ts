9cbb59103dbf8be4ed6c9f3b8f3ad311003a98ab
henriqtorresl <henriquetlandin@gmail.com>
2024-07-08 09:44:29 -0300

    logica do service de darkTheme concluida

---
+++ src/app/services/dark-theme.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DarkThemeService {
+
+  private darkTheme: boolean = false;
+
+  constructor() { }
+
+  toogleDarkTheme(): void {
+    this.darkTheme = !this.darkTheme;
+    const className: string = 'dark-mode';
+
+    if (this.darkTheme) {
+      document.body.classList.add(className);
+    } else {
+      document.body.classList.remove(className);
+    }
+  }
+
+  isDarkMode(): boolean {
+    return this.darkTheme;
+  }
+
+}
