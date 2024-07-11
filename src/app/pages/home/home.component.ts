d98dcc5ce9130ba6dadccdadd3ef4c80d42069d4
henriqtorresl <henriquetlandin@gmail.com>
2024-07-11 20:26:04 -0300

    adicionando animações

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DarkThemeService } from 'src/app/services/dark-theme.service';
 import TypeIt from "typeit";
 
