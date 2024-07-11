8c74edb71292ddf4ad5832d78b7beafe38487502
henriqtorresl <henriquetlandin@gmail.com>
2024-07-11 14:42:45 -0300

    adicionando animação no subtitulo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DarkThemeService } from 'src/app/services/dark-theme.service';
+import TypeIt from "typeit";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   constructor(
     public darkThemeService: DarkThemeService
   ) {}
+
+  ngOnInit(): void {
+    this.animateSubTitle();
+  }
+
+  animateSubTitle(): void {
+    new TypeIt('#element', {
+      strings: `Desenvolvedor Full Stack`,
+      speed: 75,
+      loop: true,
+    }).go();
+  }
 
   navigateTo(url: string): void {
     window.open(url, '_blank');
