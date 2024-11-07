d685d0d03f06eb4c814cfe707d01e25eee47b65b
Henrique Torres <henriquetlandin@gmail.com>
2024-11-07 15:16:30 -0300

    feat: adicionando experiência

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { DarkThemeService } from 'src/app/services/dark-theme.service';
-import TypeIt from "typeit";
+import TypeIt from 'typeit';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(
-    public darkThemeService: DarkThemeService
-  ) {}
+  constructor(public darkThemeService: DarkThemeService) {}
 
   ngOnInit(): void {
     this.animateSubTitle();
@@ -22,33 +19,40 @@ export class HomeComponent implements OnInit {
   }
 
   animationExperience(): void {
-    const experience = document.querySelector('.experience')!;
+    const experiences = document.querySelectorAll('.experience');
 
-    const myObserver = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        entries[0].target.classList.add('showFromDown');
+    const myObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('showFromDown');
+          }
+        });
+      },
+      {
+        threshold: 0.2,
       }
-    }, {
-      threshold: 0.2
-    });
+    );
 
-    myObserver.observe(experience);
+    experiences.forEach((experience) => myObserver.observe(experience));
   }
 
   animationGraduation(): void {
     const graduation = document.querySelector('.unb')!;
 
-    const myObserver = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        entries[0].target.classList.add('showFromRight');
+    const myObserver = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          entries[0].target.classList.add('showFromRight');
+        }
+      },
+      {
+        threshold: 0.5,
       }
-    }, {
-      threshold: 0.5
-    });
+    );
 
     myObserver.observe(graduation);
   }
-
 
   animateSubTitle(): void {
     new TypeIt('#element', {
@@ -61,5 +65,4 @@ export class HomeComponent implements OnInit {
   navigateTo(url: string): void {
     window.open(url, '_blank');
   }
-
 }
